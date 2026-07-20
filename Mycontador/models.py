8947b9ee8f6d81cd1b2c4220d4c8431281680153
Rodrigo de sousa barbosa <detonadorrodrigo11@gmail.com>
2026-07-20 15:54:11 -0300

    create modelo habito

--- Mycontador/models.py
+++ Mycontador/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class habitos(models.Model):
+    Name = models.CharField(max_length=100)
+    Description = models.CharField(max_length=100)
+    startDate = models.DateField()
+
+    def __str__(self):
+        return self.Name
+    
+      
