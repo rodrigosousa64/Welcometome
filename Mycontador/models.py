422e66a744b2a272716d395979b3568b491066a3
Rodrigo de sousa barbosa <detonadorrodrigo11@gmail.com>
2026-07-24 18:46:40 -0300

    create model of calendar weeks

--- Mycontador/models.py
+++ Mycontador/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import timedelta
 # Create your models here.
 
 class habitos(models.Model):
@@ -11,3 +11,16 @@ class habitos(models.Model):
         return self.Name
     
       
+class Calendario(models.Model):
+    nome = models.CharField(max_length=100) # Alterado de 'name' para 'nome'
+    quantidade_semanas = models.IntegerField()
+    data_inicio = models.DateField()
+    data_fim = models.DateField(editable=False, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nome} ({self.quantidade_semanas} semanas)"
+
+    def save(self, *args, **kwargs):
+        if self.data_inicio and self.quantidade_semanas is not None:
+            self.data_fim = self.data_inicio + timedelta(weeks=self.quantidade_semanas)
+        super().save(*args, **kwargs)
