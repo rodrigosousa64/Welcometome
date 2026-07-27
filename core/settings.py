eff982e3bb5fd5fb511c7241e06d828ca49002ec
Rodrigo de sousa barbosa <detonadorrodrigo11@gmail.com>
2026-07-27 20:45:13 -0300

    Fix trailing slash in CSRF_TRUSTED_ORIGINS

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ load_dotenv(BASE_DIR / '.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-CSRF_TRUSTED_ORIGINS = [ 'https://rodrigo64.up.railway.app/' ]
+CSRF_TRUSTED_ORIGINS = ['https://rodrigo64.up.railway.app']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
