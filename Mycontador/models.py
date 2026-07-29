b3b1d6f2f7c333d9e26ce0537f8d108600651c65
Rodrigo de sousa barbosa <detonadorrodrigo11@gmail.com>
2026-07-29 20:32:32 -0300

    new models camps of books

--- Mycontador/models.py
+++ Mycontador/models.py
@@ -43,7 +43,9 @@ class Livros(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     autor = models.CharField(max_length=100)
-    urlimagem = models.URLField()    
+    urlimagem = models.URLField()  
+    lido = models.BooleanField(default=False)
+    data_leitura = models.DateField(null=True, blank=True)  
 
     def __str__(self):
         return self.name
