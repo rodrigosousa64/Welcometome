8af0346ba4ac5d767ee510a66d0ebf5be1a151e2
Rodrigo de sousa barbosa <detonadorrodrigo11@gmail.com>
2026-08-02 22:19:54 -0300

    ubique ark to crud books

--- Mylivros/views.py
+++ Mylivros/views.py
@@ -2,6 +2,3 @@ from django.shortcuts import render
 from .models import Livros
 
 # Create your views here.
-def meuslivros(request):
-    livros = Livros.objects.all()
-    return render(request, "Mylivros/meuslivros.html", {"livros": livros})
