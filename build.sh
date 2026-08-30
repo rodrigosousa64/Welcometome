06cd9cc56d5300444cadb41681e3e2efe02623ff
Rodrigo de sousa barbosa <detonadorrodrigo11@gmail.com>
2026-08-30 11:26:45 -0300

    add nvm installer in build script

--- build.sh
+++ build.sh
@@ -2,6 +2,17 @@
 # Saia em caso de erro
 set -o errexit
 
+# Verifica se o NPM não existe no ambiente e instala o Node via NVM
+if ! command -v npm &> /dev/null
+then
+    echo "NPM não encontrado pelo builder. Instalando Node via NVM..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # Carrega o nvm
+    nvm install 20
+    nvm use 20
+fi
+
 echo "Instalando dependências e realizando build do Frontend..."
 cd frontend
 npm install
